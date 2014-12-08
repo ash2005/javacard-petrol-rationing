1e80db47440b64275aa3cd403dd41eacc8209af4
RomanosVM <romanos>
2014-12-08 15:58:13 +0100

    Mysql check if charged in db.

--- AraTerminal/src/ru/hwsec/teamara/Main.java
+++ AraTerminal/src/ru/hwsec/teamara/Main.java
@@ -6,25 +6,21 @@ public class Main {
 		MySql db = new MySql();
 		db.initializedb();
 		
+		
 		ChargingTerminal charging = new ChargingTerminal(db, (byte)0x21);
-		charging.connectToCard();
-		charging.use();
+		//if (charging.connectToCard())
+			charging.use();
 		
 		PetrolTerminal pumping = new PetrolTerminal((byte) 0x11);
-		pumping.use();
-		pumping.use();
-		pumping.use();
-		charging.use();
-		/*
-		short new_balance = 200;
-		byte [] balance = new byte[2];
-		balance[0] = (byte)(new_balance);
-		balance[1] = (byte)((new_balance >> 8) & 0xFF);
+		//if (pumping.connectToCard()){
+			pumping.use();
+			pumping.use();
+			pumping.use();
+		//}
 		
 		
-		new_balance = (short) ((balance[1] << 8) + (balance[0]&0xFF));
-		System.out.println("Alvin:Test");
-		System.out.println(new_balance);*/
+		//if (charging.connectToCard())
+			charging.use();
 	}
 
 }
